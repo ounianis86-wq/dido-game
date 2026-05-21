4d02c2e6eaca07ff7e0d41e367ac61ad0c9e3b3c
ounianis86-wq <286569181+ounianis86-wq@users.noreply.github.com>
2026-05-21 19:09:29 +0100

    script.js
    let score = 0;
    
    function startGame() {
        document.body.innerHTML = `
            <div style="text-align:center;padding-top:40px;font-family:Arial;">
                <h1>Dido Game 🎮</h1>
                <p id="score">النقاط: 0</p>
    
                <button onclick="addPoint()" 
                style="
                    padding:20px 40px;
                    font-size:22px;
                    border:none;
                    border-radius:15px;
                    background:black;
                    color:white;
                    cursor:pointer;
                ">
                    اضغط هنا
                </button>
            </div>
        `;
    }
    
    function addPoint() {
        score++;
    
        document.getElementById("score").innerText =
            "النقاط: " + score;
    
        if(score >= 10) {
            alert("لقد ربحت! 🎉");
        }
    }

--- script.js
+++ script.js
@@ -1,7 +1,34 @@
+let score = 0;
+
 function startGame() {
     document.body.innerHTML = `
-        <h1 style="color:black;text-align:center;margin-top:50px;">
-            اللعبة تعمل بنجاح 🎮
-        </h1>
+        <div style="text-align:center;padding-top:40px;font-family:Arial;">
+            <h1>Dido Game 🎮</h1>
+            <p id="score">النقاط: 0</p>
+
+            <button onclick="addPoint()" 
+            style="
+                padding:20px 40px;
+                font-size:22px;
+                border:none;
+                border-radius:15px;
+                background:black;
+                color:white;
+                cursor:pointer;
+            ">
+                اضغط هنا
+            </button>
+        </div>
     `;
+}
+
+function addPoint() {
+    score++;
+
+    document.getElementById("score").innerText =
+        "النقاط: " + score;
+
+    if(score >= 10) {
+        alert("لقد ربحت! 🎉");
+    }
 }
